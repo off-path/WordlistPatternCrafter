494a34ccd61bcc930f5e4d16bb441aacd8cafcd2
offpath <91066290+off-path@users.noreply.github.com>
2024-06-29 17:47:21 +0200

    fix bug

--- generate_wordlist.py
+++ generate_wordlist.py
@@ -3,37 +3,14 @@ import itertools
 import string
 from tqdm import tqdm
 
-def generate_wordlist(input_file, output_file, start_range, number_range, pattern, full_range):
+def generate_wordlist(args):
 
-    if not input_file:
-        print("You must provide a file containing the list of words.")
-        return
-
-    if not pattern:
-        print("You must provide a pattern.")
-        return
-    
-    if not output_file:
-        output_file = 'output.txt'
-
-    if 'n' in pattern and not number_range:
-        print("You must provide a range of numbers to use with the pattern.")
-        return
-
-    if number_range and (not number_range.isdigit() or int(number_range) <= 0):
-        print("The range must be a positive number.")
-        return
-
-    if start_range and (not start_range.isdigit() or int(start_range) < 0):
-        print("The start range must be a non-negative number.")
-        return
-
-    if 'n' not in pattern or 'c' not in pattern:
-        print("You must provide at least one pattern with the range.")
-        return
-
-    number_range = int(number_range)
-    start_range = int(start_range) if start_range else 0
+    input_file = args.word
+    number_range = int(args.range)
+    start_range = int(args.start_range) if args.start_range else 0
+    full_range = args.full_range
+    pattern = args.pattern
+    output_file = args.output
     special_chars = string.punctuation
     letters = string.ascii_letters
     printable = string.printable
@@ -79,11 +56,9 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Generate combined passwords.")
     parser.add_argument("-w", "--word", required=True, help="File containing the list of words.")
     parser.add_argument("-r", "--range", help="Range of numbers to add.")
-    parser.add_argument("-sr", "--start-range", type=int, help="Starting point of the number range.")
+    parser.add_argument("-sr", "--start_range", type=int, help="Starting point of the number range.")
     parser.add_argument("-fr", "--full_range", action='store_true', help="Use full range for the length of numbers.")
     parser.add_argument("-p", "--pattern", required=True, help="Pattern of the password (c for special characters, n for numbers, l for letters, p for allprintable, w for the word), example: -p=c,n,p,w,n,l")
-    parser.add_argument("-o", "--output", required=True, help="Output file to write the generated combinations.")
+    parser.add_argument("-o", "--output", help="Output file to write the generated combinations.")
 
-    args = parser.parse_args()
-
-    generate_wordlist(args.word, args.output, args.range, args.pattern, args.full_range)
+    generate_wordlist(parser.parse_args())
